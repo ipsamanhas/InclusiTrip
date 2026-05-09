77e729dd3d1f8277a261187ea688798f93eeae0a
rosannadovganyuk3 <rdovgany@ualberta.ca>
2026-05-09 14:04:14 -0600

    create account, login and welcome page html done

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+FRONTEND_DIR = BASE_DIR / "frontend"
+
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def home():
+    return FileResponse(FRONTEND_DIR / "welcome.html")
+
+
+@app.get("/login")
+async def login():
+    return FileResponse(FRONTEND_DIR / "login.html")
+
+
+@app.get("/create-account")
+async def create_account():
+    return FileResponse(FRONTEND_DIR / "create-account.html")
